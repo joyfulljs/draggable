e5a222e651306b64448493dde9d08fd4a4217a55
elvinzhu <yongyu168@foxmail.com>
2020-02-25 17:24:30 +0800

    add test case

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 # useage
 
 ```JS
-import Scalable from '@joyfulljs/scalable';
+import Scalable from '@joyfulljs/draggable';
 
 // ...
 mount(){
-  this.instance = new Scalale(this.refs.el);
+  this.instance = new Draggable(this.refs.el);
 },
 active(){
   // if need to reset to origin state
@@ -18,6 +18,44 @@ unmount(){
 }
 // ...
 
+```
+
+# api
+
+```js
+function Draggable(el: HTMLElement, options: IOptions): {
+    reset: () => void;
+    destroy: () => void;
+};
+
+interface IOptions {
+  /**
+   * triggered when dragging.
+   * return false to cancel this moving.
+   * @param e event argument { scale: number }
+   */
+  onMoving(e: {
+      deltX: number;
+      deltY: number;
+      originalEvent: TouchEvent;
+  }): boolean;
+  /**
+   * x轴正向最大拖动
+   */
+  maxX?: number;
+  /**
+   * y轴正向最大拖动
+   */
+  maxY?: number;
+  /**
+   * x轴负向最大拖动
+   */
+  minX?: number;
+  /**
+   * y轴负向最大拖动
+   */
+  minY?: number;
+}
 ```
 
 # LICENSE
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,100 @@
 
+const $ = require('jquery');
+const { default: Draggable } = require('../dist/index.c');
 
+// use jquery to bind/remove event
+const oldBinder = HTMLElement.prototype.addEventListener;
+const oldRemove = HTMLElement.prototype.removeEventListener;
 
+const newBinder = function (type, callback) {
+  this.addEventListener = oldBinder;
+  $(this).on(type, callback);
+  this.addEventListener = newBinder;
+}
+
+const newRemove = function (type, callback) {
+  this.removeEventListener = oldRemove;
+  $(this).off(type, callback);
+  this.removeEventListener = newRemove;
+}
+
+HTMLElement.prototype.addEventListener = newBinder;
+HTMLElement.prototype.removeEventListener = newRemove;
+
+window.addEventListener = newBinder;
+window.removeEventListener = newRemove;
+
+const startEvent = $.Event('mousedown', { pageX: 20, pageY: 30 });
+const moveEvent = $.Event('mousemove', { pageX: 30, pageY: 40 });
+const moveEvent2 = $.Event('mousemove', { pageX: 40, pageY: 60 });
+
+function getEl() {
+  const div = document.createElement('div');
+  div.style.width = '100px';
+  div.style.height = '100px';
+  document.body.appendChild(div);
+  return div;
+}
+
+test('drag correctly', () => {
+  const el = getEl();
+  new Draggable(el);
+
+  $(el).trigger(startEvent);
+  $(window).trigger(moveEvent);
+
+  const trans = el.style.transform;
+  expect(trans).toBe('matrix(1,0,0,1,10,10)')
+})
+
+test('trigger onMoving correctly', () => {
+
+  const el = getEl();
+  let eventArg;
+
+  new Draggable(el, {
+    onMoving(e) {
+      eventArg = e
+    }
+  });
+
+  $(el).trigger(startEvent);
+
+  $(window).trigger(moveEvent);
+  expect(eventArg.deltX).toBe(10);
+  expect(eventArg.deltY).toBe(10)
+
+  $(window).trigger(moveEvent2);
+  expect(eventArg.deltX).toBe(20);
+  expect(eventArg.deltY).toBe(30)
+})
+
+test('treat maxX/maxY correctly', () => {
+  const el = getEl();
+  new Draggable(el, {
+    maxX: 5,
+    maxY: 0
+  });
+
+  $(el).trigger(startEvent);
+  $(window).trigger(moveEvent);
+
+  const trans = el.style.transform;
+  expect(trans).toBe('matrix(1,0,0,1,5,0)')
+})
+
+test('treat minX/minY correctly', () => {
+  const el = getEl();
+  new Draggable(el, {
+    minX: -5,
+    minY: 0
+  });
+
+  $(el).trigger(startEvent);
+  $(window).trigger(
+    $.Event('mousemove', { pageX: -10, pageY: -20 })
+  );
+
+  const trans = el.style.transform;
+  expect(trans).toBe('matrix(1,0,0,1,-5,0)')
+})
